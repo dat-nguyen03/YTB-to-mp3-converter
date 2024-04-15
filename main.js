648c87cac467e8eae0406e4c08729c487cfd284a
Dat Nguyen <datnguyen01122003@gmail.com>
2024-04-15 16:03:21 +0700

    init

---
+++ main.js
@@ -0,0 +1,87 @@
+window.addEventListener("load", function () {
+  const loader = document.querySelector(".loader-container");
+  const inputValue = document.querySelector("#url");
+  const btnSubmit = document.querySelector("#btn-submit");
+  const resultContainer = document.querySelector("#resultContainer");
+  const videoTitle = document.querySelector("#videoTitle");
+  const downloadLink = document.querySelector("#downloadLink");
+  const size = document.querySelector("#size");
+  const regex = new RegExp(
+    "^(https?://)?((www|music)\\.)?(youtube|youtu|youtube-nocookie)\\.(com|be)/"
+  );
+
+  btnSubmit.addEventListener("click", async function () {
+    if (inputValue.value === "") {
+      errorHandle("Vui lòng nhập URL!").then(() => {
+        inputValue.focus();
+      });
+      return;
+    }
+    if (!regex.test(inputValue.value)) {
+      errorHandle("Vui lòng nhập đúng URL!").then(() => {
+        inputValue.focus();
+      });
+      return;
+    }
+    let parts = inputValue.value.split(/[=&]/);
+    let videoId = parts[1];
+    mp3Conversion(videoId);
+  });
+
+  const mp3Conversion = async (id) => {
+    try {
+      loader.classList.remove("hidden");
+      let url = `https://youtube-mp36.p.rapidapi.com/dl?id=${id}`;
+      const options = {
+        method: "GET",
+        headers: {
+          async: true,
+          crossDomain: true,
+          "X-RapidAPI-Key":
+            "9760a4eb7cmshd896bf44e044861p117162jsnf7d28979cc12",
+          "X-RapidAPI-Host": "youtube-mp36.p.rapidapi.com",
+        },
+      };
+      let res = await fetch(url, options).then((response) => response.json());
+
+      if (res.status == "processing") {
+        this.setTimeout(async () => {
+          mp3Conversion(id);
+        }, 1000);
+      }
+
+      if (res.status === "fail") {
+        errorHandle("Có lỗi xảy ra. Vui lòng thử lại!").then(() => {
+          loader.classList.add("hidden");
+        });
+        return;
+      }
+      if (res.status === "ok") {
+        resultContainer.classList.remove("hidden");
+        videoTitle.textContent = res.title;
+        downloadLink.href = res.link;
+        size.textContent = res.size;
+        loader.classList.add("hidden");
+        successHandle("Đã sẵn sàng để tải xuống!");
+      }
+    } catch (error) {
+      console.log(error);
+      errorHandle("Có lỗi xảy ra. Vui lòng thử lại!").then(() => {
+        loader.classList.add("hidden");
+      });
+    }
+  };
+
+  const errorHandle = (error) => {
+    return Swal.fire({
+      icon: "error",
+      title: error,
+    });
+  };
+  const successHandle = (message) => {
+    return Swal.fire({
+      icon: "success",
+      title: message,
+    });
+  };
+});
